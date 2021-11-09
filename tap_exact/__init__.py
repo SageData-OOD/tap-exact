15f3f359b0060d9f3a238405c618cf7e875be949
Dimitar Petrunov <dimitar.petrunov@sagedata.net>
2021-11-09 16:20:52 +0200

    TransactionLines + ReceivablesList streams

--- tap_exact/__init__.py
+++ tap_exact/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import json
-import re
+
 
 import backoff
 import requests
@@ -11,6 +11,7 @@ from singer import utils, metadata
 from singer.catalog import Catalog, CatalogEntry
 from singer.transform import transform
 from datetime import datetime, timedelta
+from utils import *
 
 REQUIRED_CONFIG_KEYS = ["start_date", "host_url", "client_id", "client_secret", "refresh_token"]
 LOGGER = singer.get_logger()
@@ -26,90 +27,10 @@ URIS = {
     "purchase_invoices": "/api/v1/{division}/purchase/PurchaseInvoices",
     "gl_accounts": "/api/v1/{division}/financial/GLAccounts",
     "bank_entry_lines": "/api/v1/{division}/financialtransaction/BankEntryLines",
-    "general_journal_entry_lines": "/api/v1/{division}/generaljournalentry/GeneralJournalEntryLines"
+    "general_journal_entry_lines": "/api/v1/{division}/generaljournalentry/GeneralJournalEntryLines",
+    "transaction_lines": "/api/v1/{division}/financialtransaction/TransactionLines",
+    "receivables_list": "/api/v1/{division}/read/financial/ReceivablesList"
 }
-
-
-class ExactRateLimitError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-        super().__init__(self.msg)
-
-
-def get_abs_path(path):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
-
-
-def camel_to_snake_case(name):
-    """
-    AssimilatedVatBox  --> assimilated_vat_box
-    """
-    sn = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
-    sn = sn.replace("i_d", "id")
-    sn = sn.replace("d_c", "dc")
-    sn = sn.replace("f_c", "fc")
-    sn = sn.replace("v_a_t", "vat")
-    sn = sn.replace("w_b_s", "wbs")
-    sn = sn.replace("g_l", "gl")
-    return sn
-
-
-def snake_to_camelcase(name):
-    """
-    assimilated_vat_box  -->  AssimilatedVATBox
-    """
-    replacer = {
-        "id": "ID",
-        "dc": "DC",
-        "vat": "VAT",
-        "wbs": "WBS",
-        "fc": "FC",
-        "gl": "GL"
-    }
-    exceptions = {"amount_discount_excl_vat": "AmountDiscountExclVat", "amount_fc_excl_vat": "AmountFCExclVat"}
-    # for expanded attributes e.x. [ other_percentages/id --> OtherPercentages/ID ]
-    if "/" in name:
-        spl = name.split("/")
-        return snake_to_camelcase(spl[0]) + "/" + snake_to_camelcase(spl[1])
-
-    if name not in exceptions:
-        cm = []
-        for part in name.split("_"):
-            cm.append(replacer.get(part) if replacer.get(part) else part.title())
-        return "".join(cm)
-    else:
-        return exceptions[name]
-
-
-def fix_if_datetime(val):
-    if isinstance(val, str) and val.startswith("/Date(") and val.endswith(")/"):
-        time_stamp = val.replace("/Date(", "").replace(")/", "")
-        your_dt = datetime.utcfromtimestamp(int(time_stamp) / 1000)
-        your_dt.strftime("%Y-%m-%d %H:%M:%S")
-        return str(your_dt)
-    else:
-        return val
-
-
-def arrange_records(stream_id, row):
-    if "__metadata" in row: row.pop("__metadata")
-    if stream_id == "gl_accounts" and row.get("DeductibilityPercentages"):
-        row["DeductibilityPercentages"] = row.get("DeductibilityPercentages", {}).get("results", {})
-    return row
-
-
-def refactor_record_according_to_schema(record):
-    """
-    e.x. {"AssimilatedVatBox": 12}  -->  {"assimilated_vat_box": 12}
-
-    e.x. {"AssimilatedVatBox": {"OtherData": "jon doe"}}
-    -->  {"assimilated_vat_box": {"other_data": "jon doe"}}
-    """
-    if isinstance(record, list):
-        return [refactor_record_according_to_schema(r) for r in record]
-    converted_data = {camel_to_snake_case(k): fix_if_datetime(v) if not isinstance(v, (dict, list)) else refactor_record_according_to_schema(v)
-                      for k, v in record.items()}
-    return converted_data
 
 
 # def fetch_deferred_data_if_available(records, expand_attr, stream_id, config):
@@ -133,7 +54,9 @@ def get_key_properties(stream_id):
         "purchase_invoices": ["id"],
         "gl_accounts": ["id"],
         "bank_entry_lines": ["id"],
-        "general_journal_entry_lines": ["id"]
+        "general_journal_entry_lines": ["id"],
+        "transaction_lines": ["id"],
+        "receivables_list": ["hid"],
     }
     return key_properties.get(stream_id, [])
 
@@ -144,7 +67,8 @@ def get_bookmark_attributes(stream_id):
         "purchase_invoices": "modified",
         "gl_accounts": "modified",
         "bank_entry_lines": "modified",
-        "general_journal_entry_lines": "modified"
+        "general_journal_entry_lines": "modified",
+        "transaction_lines": "modified",
     }
     return bookmark.get(stream_id)
 
